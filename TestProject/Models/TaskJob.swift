121ef3bce2e4e9d5dbd7585cc936a342929a8096
Archimedes Angeles <pachie@archimedess-macbook-pro.local>
2019-02-04 04:25:47 +0800

    add table and cells

---
+++ TestProject/Models/TaskJob.swift
@@ -0,0 +1,40 @@
+//
+//  TaskJob.swift
+//  TestProject
+//
+//  Created by Archimedes Angeles on 4/2/19.
+//  Copyright © 2019 Archimedes Angeles. All rights reserved.
+//
+
+import Foundation
+
+struct TaskJob {
+    var name: String
+    var scheduleType: TaskJobScheduleType
+    var status: TaskJobStatus
+    var jobID: String
+    var location: String
+    var expectedTimingFrom: String
+    var expectedTimingTo: String
+    var isMondayEnabled: Bool
+    var isTuesdayEnabled: Bool
+    var isWednesdayEnabled: Bool
+    var isThursdayEnabled: Bool
+    var isFridayEnabled: Bool
+    var isSaturdayEnabled: Bool
+    var isSundayEnabled: Bool
+}
+
+enum TaskJobStatus: String
+{
+    case Success
+    case Failed
+    case NotSet
+    
+}
+
+enum TaskJobScheduleType: String
+{
+    case Daily
+    case Monthly
+}
---
+++ TestProject/New Group/JobCellTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  JobCellTableViewCell.swift
+//  TestProject
+//
+//  Created by Archimedes Angeles on 4/2/19.
+//  Copyright © 2019 Archimedes Angeles. All rights reserved.
+//
+
+import UIKit
+
+class JobCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var btnImageIcon: UIImageView!
+    @IBOutlet weak var lblJobLabel: UILabel!
+    @IBOutlet weak var lblJobTiming: UILabel!
+    var jobId : String!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func fillLabels(taskDetail: TaskJob)
+    {
+        lblJobLabel.text = taskDetail.name
+        lblJobTiming.text = taskDetail.expectedTimingFrom
+        jobId = taskDetail.jobID
+    }
+
+}

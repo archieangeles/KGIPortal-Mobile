191656a7b2294b73e74dfcfedf01513648bb2980
Archimedes Angeles <pachie@Archimedess-MBP.DUTERTEDOMAIN>
2019-03-21 02:42:42 +0800

    Add protocol

---
+++ SampleCoreData/Controller/PersonListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PersonListViewController.swift
+//  SampleCoreData
+//
+//  Created by Archimedes Angeles on 20/3/19.
+//  Copyright © 2019 Archimedes Angeles. All rights reserved.
+//
+
+import UIKit
+
+class PersonListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SampleCoreData/SampleCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleCoreData
+//
+//  Created by Archimedes Angeles on 17/3/19.
+//  Copyright © 2019 Archimedes Angeles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
